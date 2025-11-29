0a621eb84ece6df947c88558f3f7d6221a333f7a
0xtochee <mac@0xtochee-MacBook-Pro.local>
2025-11-29 19:43:49 +0100

    update app jsx file and add search jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Search } from './components/Search';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className='pattern' />
@@ -8,8 +11,14 @@ const App = () => {
       <div className='wrapper'>
 
         <header>
+          <img src="./hero.png" alt="Hero Banner" />
+
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy without Hassle</h1>
         </header>
+        
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className='text-white'>{searchTerm}</h1>
+
       </div>
     </main>
   );
